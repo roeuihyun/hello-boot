f49bcf3351a5ba7975f33e44fec5dd8a768c66f2
roeuihyun <roeuihyun@gmail.com>
2021-07-11 18:23:36 +0900

    Add Gson & User Entity Add

---
+++ src/main/java/roeuihyun/rest/entity/User.java
@@ -0,0 +1,39 @@
+package roeuihyun.rest.entity;
+
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    //
+    private String id;
+    private String name;
+    private String email;
+
+    //
+    public User(){
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public User(String name, String email){
+        this();
+        this.name = name;
+        this.email = email;
+    }
+
+    public static User sample(){
+        return new User("roeuihyun", "roeuihyun@gmail.com");
+    }
+
+    public static void main(String[] args) {
+        User user = new User("kim","kim@naver.com;");
+        System.out.println(new Gson().toJson(user));
+    }
+
+}
