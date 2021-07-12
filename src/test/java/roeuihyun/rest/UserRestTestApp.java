e9c99f5aba7be6ce15706bbd877de629b684dfd1
roeuihyun <roeuihyun@gmail.com>
2021-07-12 19:45:57 +0900

    Test Class Write

---
+++ src/test/java/roeuihyun/rest/UserRestTestApp.java
@@ -0,0 +1,13 @@
+//package roeuihyun.rest;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class UserRestTestApp {
+//    //
+//    public static void main(String[] args) {
+//        //
+//        SpringApplication.run(UserRestTestApp.class, args);
+//    }
+//}
